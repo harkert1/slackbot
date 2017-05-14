519ab869e1970a1cf461ececb2072027007eb5dc
Thomas Harker <harkert2@students.rowan.edu>
2017-05-14 12:50:07 -0400

    Added teemo auto-reaction to messages with any word in word_list

--- starterbot.py
+++ starterbot.py
@@ -9,6 +9,7 @@ BOT_ID = os.environ.get("BOT_ID")
 # constants
 AT_BOT = "<@" + BOT_ID + ">"
 EXAMPLE_COMMAND = "do"
+WORD_LIST = ['cancer', 'ebola', 'teemo']
 
 # instantiate Slack & Twilio clients
 slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
@@ -23,10 +24,13 @@ def handle_command(command, channel):
     response = "Not sure what you mean. Use the *" + EXAMPLE_COMMAND + \
                "* command with numbers, delimited by spaces."
     if command.startswith(EXAMPLE_COMMAND):
-        response = "Sure...write some more code then I can do that!"
+        response = "do it yourself"
     slack_client.api_call("chat.postMessage", channel=channel,
                           text=response, as_user=True)
 
+
+def add_reax(channel,timestamp):
+    slack_client.api_call("reactions.add", name='ebola', channel=channel, timestamp=timestamp, as_user=True)
 
 def parse_slack_output(slack_rtm_output):
     """
@@ -37,11 +41,15 @@ def parse_slack_output(slack_rtm_output):
     output_list = slack_rtm_output
     if output_list and len(output_list) > 0:
         for output in output_list:
-            if output and 'text' in output and AT_BOT in output['text']:
-                # return text after the @ mention, whitespace removed
-                return output['text'].split(AT_BOT)[1].strip().lower(), \
-                       output['channel']
-    return None, None
+            if output and 'text' in output:
+                if AT_BOT in output['text']:
+                    # return text after the @ mention, whitespace removed
+                    return output['text'].split(AT_BOT)[1].strip().lower(), \
+                       output['channel'], 'command', None
+                for word in WORD_LIST:
+                    if word in output['text']:
+                        return output['text'], output['channel'], 'reaction', output['ts']
+    return None, None, None, None
 
 
 if __name__ == "__main__":
@@ -49,9 +57,11 @@ if __name__ == "__main__":
     if slack_client.rtm_connect():
         print("StarterBot connected and running!")
         while True:
-            command, channel = parse_slack_output(slack_client.rtm_read())
-            if command and channel:
+            command, channel, type, timestamp = parse_slack_output(slack_client.rtm_read())
+            if command and channel and type == 'command':
                 handle_command(command, channel)
+            elif command and channel and timestamp and type == 'reaction':
+                add_reax(channel, timestamp)
             time.sleep(READ_WEBSOCKET_DELAY)
     else:
         print("Connection failed. Invalid Slack token or bot ID?")
